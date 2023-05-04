36a43e4a7504ac92fdd8c2c2305956892c95309c
Mariela Coluccio <marielaandreacoluccio@yahoo.com.ar>
2023-05-04 20:42:05 -0300

    Natbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h3>Tienda Online</h3>
+            <div> 
+                <button>Pantalones </button>
+                <button> Remeras </button>
+                <button>Tops </button>
+            </div>
+
+        </nav>
+    )
+}
+
+export default NavBar
